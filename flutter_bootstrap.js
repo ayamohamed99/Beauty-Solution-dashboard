a8cc84e9d49e6d388f47a418f7dd9e0917155650
Aya Mohamed <aya199taha@gmail.com>
2024-07-10 15:51:03 +0300

    new deployed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "184549640"
+    serviceWorkerVersion: "3150602289"
   }
 });
